bd6d73a968f3a3ab7accf2d733b49b8d215efca1
Douglas Duarte <douglasduarte.eng@gmail.com>
2024-02-01 06:15:26 -0300

    feat: add font and theme to storybook.

--- packages/docs/.storybook/preview.ts
+++ packages/docs/.storybook/preview.ts
@@ -2,6 +2,19 @@ import type { Preview } from "@storybook/react";
 
 const preview: Preview = {
   parameters: {
+    backgrounds: {
+      default: 'dark',
+      values: [
+        {
+          name: 'light',
+          value: '#F8F8F8'
+        },
+        {
+          name: 'dark',
+          value: '#333333'
+        }
+      ]
+    },
     actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
       matchers: {
